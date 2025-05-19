f93014ce59fc6dcd8ffa7f40405490b8fd7721ac
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-05-19 19:35:31 +0000

    add 15-find-large.sh

---
+++ Practical File/Shell Scripts/15-find-large.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script to Extract ERROR messages using journalctl
+OUTPUT="/tmp/error_log_report.txt"
+journalctl -p err -b > "$OUTPUT"
+echo "Error messages saved from journalctl to: $OUTPUT"
